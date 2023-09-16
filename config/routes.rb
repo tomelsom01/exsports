6062dcd50a16c2b5771c9afbf0045eba76b1250f
tomelsom <te28@hotmail.com>
2023-09-16 22:17:25 +0100

    created controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "pages#home"
+  root to: 'pages#home'
+  get 'users', to: 'users#index'
 end
